bb0c0cd51eb328b018c9c5f9a80189bf76adc781
norayogeshwar96 <norayogeshwar96>
2022-03-15 14:23:16 -0700

    Files hinzufügen

--- agents/MCTS/monte_carlo_ts.py
+++ agents/MCTS/monte_carlo_ts.py
@@ -75,26 +75,6 @@ def expand(node):
 
     return None
 
-
-'''# select most promising node
-def select(self, node):
-    # make sure that we're dealing with non-terminal nodes
-    while not node.is_terminal:
-        # case where the node is fully expanded
-        if node.fully_expanded:
-            node = self.get_best_move(node, 2)
-
-        # case where the node is not fully expanded
-        else:
-            # otherwise expand the node
-            return self.expand(node)
-
-        # return node
-        return node
-
-'''
-
-
 def current_player(player: BoardPiece):
     if player == PLAYER1:
         return 1
@@ -214,49 +194,3 @@ def select(node):
     # return node
     return node
 
-
-'''
-def monte_carlo_tree_search(root: node):
-    while resources_left(time, computational power):
-        leaf = traverse(root)
-        simulation_result = rollout(leaf)
-        backpropagate(leaf, simulation_result)
-
-    return best_child(root)
-
-
-# function for node traversal
-def traverse(node):
-    while fully_expanded(node):
-        node = best_uct(node)
-
-    # in case no children are present / node is terminal
-    return pick_univisted(node.children) or node
-
-
-# function for the result of the simulation
-def rollout(node):
-    while non_terminal(node):
-        node = rollout_policy(node)
-    return result(node)
-
-
-# function for randomly selecting a child node
-def rollout_policy(node):
-    return pick_random(node.children)
-
-
-# function for backpropagation
-def backpropagate(node, result):
-    if is_root(node) return
-    node.stats = update_stats(node, result)
-    backpropagate(node.parent)
-
-
-# function for selecting the best child
-# node with highest number of visits
-def best_child(node):
-    pick
-    child
-    with highest number of visits
-'''
